1c93fad5e1fed6c8253a35a3eca70b55a2f97257
mt26691 <tung.nguyen@scopicsoftware.com>
2020-04-22 14:19:38 +0700

    add redis

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const http = require('http');
-
 const app = express();
+const redis = require("redis");
+
+
+const redisClient = redis.createClient({
+  host: '127.0.0.1',
+  port: 6379
+});
+
+redisClient.on("ready", function(error) {
+  console.log(`connected to redis`)
+});
 
 app.get('/', (req, res) => {
   res.send(`Hello Docker!`);
